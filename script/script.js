3094a6250041538ef0658758ce52aec0df64633b
jowaipolytechnic <jowaipolytechnic@yahoo.com>
2015-05-21 10:06:23 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -50,7 +50,7 @@ function xhr(mode,payload){
 		}
 		//var jsn=payload;//formJSON(payload);
 		var pLoad=JSON.stringify(payload);
-		xmlhttp.open("POST",'https://script.google.com/macros/s/AKfycbzZ-2K0zQJM8BOi1vgckZyxD5nOznBPcE-FUJioaTuGJKKwjog/exec?mode='+mode+'&payload='+pLoad,true);
+		xmlhttp.open("POST",'https://script.google.com/macros/s/AKfycbyQ-8t4vfPH8lrrHUP_T3ohPEEEzhtxw0L1F-kTBYl5zcrmbQVU/exec?mode='+mode+'&payload='+pLoad,true);
 		xmlhttp.send();
 		// Timeout to abort in 5 seconds
 		var xmlHttpTimeout=setTimeout(ajaxTimeout,60000);
